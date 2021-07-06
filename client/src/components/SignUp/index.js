c1f4b0600069675991e43a85fb72272f09a8e5e4
alvinoalvin <alvinoalvin@gmail.com>
2021-07-05 19:33:37 -0700

    adding snackbar and redirect for signup

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -5,8 +5,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -16,6 +14,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import axios from "axios";
 import Snackbar from '@material-ui/core/Snackbar';
+import { useHistory } from "react-router-dom";
 
 import Alert from '../../helpers/Components/Alert.js'
 import '../../styles/variables.scss';
@@ -33,9 +32,6 @@ function Copyright() {
     </Typography>
   );
 }
-// function Alert(props) {
-//   return <MuiAlert elevation={6} variant="filled" {...props} />;
-// }
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
@@ -65,6 +61,11 @@ export default function SignUp() {
 
   const { team_id } = useContext(authContext);
   const teamID = team_id;
+  const history = useHistory();
+
+  const redirect = () => {
+    history.push('/allSkills')
+  }
 
   const [alert, setAlert] = useState({
     message: '',
@@ -92,13 +93,16 @@ export default function SignUp() {
       email: email,
       password: password
     }
-    console.log("hi: ", newUser)
+
     return axios.post(`api/users/signup`, newUser)
       .then(function(response) {
-        alert(`New User created for email ${newUser.email}`);
+        setAlert({ message: `New User created for email ${newUser.email}`, severity: 'success' });
+        setSnack(true);
+        setTimeout(function() { redirect() }, 1000)
+
         /* TODO
           - set context variable on, and login on sign up
-          - implement sign up
+
         */
       })
       .catch(function(error) {
@@ -200,6 +204,8 @@ export default function SignUp() {
                 setAlert({ message: 'Please Enter A Password', severity: 'error' })
                 setSnack(true)
               } else {
+                console.log("hi")
+                event.preventDefault()
                 addUser()
               }
             }}
